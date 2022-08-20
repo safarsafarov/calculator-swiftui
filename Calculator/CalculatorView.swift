367d4dfdac88d4cbad5d133b1814c0bc32b9f9b2
Safar Safarov <Safarov.safar@hotmail.com>
2022-08-20 15:19:58 +0500

    ✨

---
+++ Calculator/CalculatorView.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorView.swift
+//  Calculator
+//
+//  Created by Safar Safarov on 20/08/22.
+//
+
+import SwiftUI
+
+struct CalculatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalculatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculatorView()
+    }
+}
